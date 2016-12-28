cb9631fdb7c75d45ec500a8eadf340998accad71
Onion <759308541@qq.com>
2016-12-28 16:02:34 +0800

    asd

--- app/src/main/java/com/example/zhangqi/onion/MainActivity.java
+++ app/src/main/java/com/example/zhangqi/onion/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.zhangqi.onion;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
